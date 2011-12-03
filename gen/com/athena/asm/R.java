60ba8e1c7fbb20b69c91184b765a099b5fb3a659
xi.lin <linxi60@gmail.com>
2011-12-04 00:51:43 +0800

    尝试修正发表按钮显示不全的问题

--- gen/com/athena/asm/R.java
+++ gen/com/athena/asm/R.java
@@ -249,10 +249,10 @@ public final class R {
         public static final int write_mail=0x7f03001a;
     }
     public static final class string {
-        public static final int about_content=0x7f07002c;
+        public static final int about_content=0x7f07002d;
         /**  关于 
          */
-        public static final int about_title=0x7f07002b;
+        public static final int about_title=0x7f07002c;
         public static final int app_name=0x7f070000;
         /**  分区 
          */
@@ -287,55 +287,56 @@ public final class R {
         public static final int next_page=0x7f070015;
         /**  post相关 
          */
-        public static final int post_alert_title=0x7f070025;
-        public static final int post_copy_author=0x7f070028;
-        public static final int post_query_author=0x7f070029;
-        public static final int post_reply_mail=0x7f070027;
-        public static final int post_reply_post=0x7f070026;
+        public static final int post_alert_title=0x7f070026;
+        public static final int post_copy_author=0x7f070029;
+        public static final int post_query_author=0x7f07002a;
+        public static final int post_reply_mail=0x7f070028;
+        public static final int post_reply_post=0x7f070027;
         public static final int pre_page=0x7f070014;
-        public static final int profile_alive_number=0x7f07001b;
-        public static final int profile_login_count=0x7f07001c;
-        public static final int profile_online_status=0x7f07001e;
-        public static final int profile_post_count=0x7f07001d;
+        public static final int profile_alive_number=0x7f07001c;
+        public static final int profile_login_count=0x7f07001d;
+        public static final int profile_online_status=0x7f07001f;
+        public static final int profile_post_count=0x7f07001e;
         /**  资料页 
          */
-        public static final int profile_send_mail=0x7f07001a;
+        public static final int profile_send_mail=0x7f07001b;
         /**  下拉刷新 
          */
-        public static final int pull_to_refresh_pull_label=0x7f07003b;
-        public static final int pull_to_refresh_refreshing_label=0x7f07003d;
-        public static final int pull_to_refresh_release_label=0x7f07003c;
-        public static final int pull_to_refresh_tap_label=0x7f07003e;
-        public static final int setting_auto_login=0x7f07002f;
-        public static final int setting_auto_login_details=0x7f070030;
-        public static final int setting_black_list=0x7f070039;
-        public static final int setting_black_list_details=0x7f07003a;
-        public static final int setting_default_board_type=0x7f070033;
-        public static final int setting_default_board_type_details=0x7f070034;
-        public static final int setting_default_tab=0x7f070031;
-        public static final int setting_default_tab_details=0x7f070032;
-        public static final int setting_guidance_font_size=0x7f070035;
-        public static final int setting_guidance_second_font_size=0x7f070036;
-        public static final int setting_post_font_size=0x7f070038;
+        public static final int pull_to_refresh_pull_label=0x7f07003c;
+        public static final int pull_to_refresh_refreshing_label=0x7f07003e;
+        public static final int pull_to_refresh_release_label=0x7f07003d;
+        public static final int pull_to_refresh_tap_label=0x7f07003f;
+        public static final int setting_auto_login=0x7f070030;
+        public static final int setting_auto_login_details=0x7f070031;
+        public static final int setting_black_list=0x7f07003a;
+        public static final int setting_black_list_details=0x7f07003b;
+        public static final int setting_default_board_type=0x7f070034;
+        public static final int setting_default_board_type_details=0x7f070035;
+        public static final int setting_default_tab=0x7f070032;
+        public static final int setting_default_tab_details=0x7f070033;
+        public static final int setting_guidance_font_size=0x7f070036;
+        public static final int setting_guidance_second_font_size=0x7f070037;
+        public static final int setting_post_font_size=0x7f070039;
         /**  设置 
          */
-        public static final int setting_remember_user=0x7f07002d;
-        public static final int setting_remember_user_details=0x7f07002e;
-        public static final int setting_subject_font_size=0x7f070037;
-        public static final int title_category=0x7f070022;
-        public static final int title_favorite=0x7f070020;
+        public static final int setting_remember_user=0x7f07002e;
+        public static final int setting_remember_user_details=0x7f07002f;
+        public static final int setting_subject_font_size=0x7f070038;
+        public static final int title_category=0x7f070023;
+        public static final int title_favorite=0x7f070021;
         /**  标题栏文字 
          */
-        public static final int title_guidance=0x7f07001f;
-        public static final int title_mail=0x7f070021;
-        public static final int title_profile=0x7f070023;
-        public static final int title_search_post=0x7f070024;
+        public static final int title_guidance=0x7f070020;
+        public static final int title_mail=0x7f070022;
+        public static final int title_profile=0x7f070024;
+        public static final int title_search_post=0x7f070025;
+        public static final int topic_all_page=0x7f07001a;
         /**  普通模式下导航 
          */
         public static final int topic_first_page=0x7f070017;
         public static final int topic_next_page=0x7f070019;
         public static final int topic_pre_page=0x7f070018;
-        public static final int write_label_post=0x7f07002a;
+        public static final int write_label_post=0x7f07002b;
     }
     public static final class style {
         public static final int Button=0x7f080014;
