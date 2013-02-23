bc203db2c913a539888d88737418e7f3c8657211
zhengfa dang <dantifer@gmail.com>
2013-02-23 12:46:15 +0800

    refactor full screen image viewer

--- src/com/athena/asm/FullImageActivity.java
+++ src/com/athena/asm/FullImageActivity.java
@@ -7,21 +7,27 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-import android.app.ProgressDialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.view.View.OnLongClickListener;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuItem;
 import com.athena.asm.util.StringUtility;
 import com.athena.asm.view.TouchImageView;
 import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 
-public class FullImageActivity extends SherlockActivity {
+public class FullImageActivity extends SherlockActivity
+	implements OnLongClickListener {
 
 	private TouchImageView m_image;
 	private String m_imageName;
@@ -29,6 +35,9 @@ public class FullImageActivity extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		setTheme(aSMApplication.THEME);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.full_image);
@@ -39,94 +48,97 @@ public class FullImageActivity extends SherlockActivity {
 		}
 		m_imageUrl = getIntent().getStringExtra(StringUtility.IMAGE_URL);
 
-		setTitle(m_imageName);
-
 		m_image = (TouchImageView) findViewById(R.id.image_view);
+		m_image.setOnLongClickListener(this);
 
 		UrlImageViewHelper.setUseZoomIn(true); // enable zoom in
 		UrlImageViewHelper.setUseZoomOut(false); // don't zoom out
 		UrlImageViewHelper.setMaxImageSize(0); // load all size
-
-		UrlImageViewHelper.setUrlDrawable(m_image, m_imageUrl);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// super.onCreateOptionsMenu(menu);
-		boolean isLight = aSMApplication.THEME == R.style.Theme_Sherlock_Light;
-
-		menu.add(0, Menu.FIRST, Menu.NONE, "保存")
-				.setIcon(isLight ? R.drawable.save_inverse : R.drawable.save)
-				.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// super.onOptionsItemSelected(item);
-		switch (item.getItemId()) {
-		case Menu.FIRST:
-			ProgressDialog pdialog = new ProgressDialog(this);
-			pdialog.setMessage("正在保存中...");
-			pdialog.show();
-			try {
-				String fileName;
-				if (m_imageName.equals("未命名")) {
-					DateFormat formatter = new SimpleDateFormat(
-							"yyyy-MM-dd-HH-mm-ss");
-					String time = formatter.format(new Date());
-					fileName = time + m_imageName + ".png";
-				} else {
-					fileName = m_imageName;
-				}
-
-				if (Environment.getExternalStorageState().equals(
-						Environment.MEDIA_MOUNTED)) {
-					String path = Environment.getExternalStorageDirectory().getPath() + "/aSM/images/";
-					File dir = new File(path);
-					if (!dir.exists()) {
-						dir.mkdirs();
-					}
-
-					FileInputStream fis = this.openFileInput(UrlImageViewHelper
-							.getFilenameForUrl(m_imageUrl));
-					FileOutputStream fos = new FileOutputStream(path + fileName);
-
-					BufferedInputStream bufr = new BufferedInputStream(fis);
-					BufferedOutputStream bufw = new BufferedOutputStream(fos);
-
-					int len = 0;
-
-					byte[] buf = new byte[1024];
-					while ((len = bufr.read(buf)) != -1) {
-						bufw.write(buf, 0, len);
-
-						bufw.flush();
-
-					}
-
-					bufw.close();
-					bufr.close();
-
-					Toast.makeText(this, "已保存到SD卡的aSM/images目录下.",
-							Toast.LENGTH_SHORT).show();
-				}
-			} catch (Exception e) {
-				Toast.makeText(this, "保存时出现未知错误.", Toast.LENGTH_SHORT).show();
-			}
-			pdialog.cancel();
-			break;
-		default:
-			break;
+		if (aSMApplication.getCurrentApplication().isNightTheme()) {
+			UrlImageViewHelper.setUrlDrawable(m_image, m_imageUrl, R.drawable.loading_night);
+		} else {
+			UrlImageViewHelper.setUrlDrawable(m_image, m_imageUrl, R.drawable.loading_day);
 		}
-		return true;
+
+		Toast.makeText(this, "长按出菜单,返回键退出", Toast.LENGTH_SHORT).show();
+
+		setRequestedOrientation(aSMApplication.ORIENTATION);
 	}
+
+
+	@Override
+	public boolean onLongClick(View arg0) {
+			List<String> itemList = new ArrayList<String>();
+			itemList.add(getString(R.string.full_image_save));
+			itemList.add(getString(R.string.full_image_back));
+			final String[] items = new String[itemList.size()];
+			itemList.toArray(items);
+
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(String.format("图片: %s", m_imageName));
+			builder.setItems(items, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int item) {
+					switch (item) {
+					case 0:
+						try {
+							String fileName;
+							if (m_imageName.equals("未命名")) {
+								DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+								String time = formatter.format(new Date());
+								fileName = time + m_imageName + ".png";
+							} else {
+								fileName = m_imageName;
+							}
+
+							if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+								String path = Environment.getExternalStorageDirectory().getPath() + "/aSM/images/";
+								File dir = new File(path);
+								if (!dir.exists()) {
+									dir.mkdirs();
+								}
+
+								FileInputStream fis = openFileInput(UrlImageViewHelper.getFilenameForUrl(m_imageUrl));
+								FileOutputStream fos = new FileOutputStream(path + fileName);
+
+								BufferedInputStream bufr = new BufferedInputStream(fis);
+								BufferedOutputStream bufw = new BufferedOutputStream(fos);
+
+								int len = 0;
+								byte[] buf = new byte[1024];
+								while ((len = bufr.read(buf)) != -1) {
+									bufw.write(buf, 0, len);
+									bufw.flush();
+								}
+								bufw.close();
+								bufr.close();
+
+								Toast.makeText(FullImageActivity.this, "图片已存为SD卡下aSM/images/" + fileName,
+										Toast.LENGTH_SHORT).show();
+							}
+						} catch (Exception e) {
+							Toast.makeText(FullImageActivity.this, "保存时出现未知错误.", Toast.LENGTH_SHORT).show();
+						}
+						break;
+					case 1:
+						onBackPressed();
+						break;
+					default:
+						break;
+					}
+					dialog.dismiss();
+				}
+			});
+
+			AlertDialog menuDialog = builder.create();
+			menuDialog.show();
+
+			return true;
+		}
 
 	@Override
 	public void onBackPressed() {
 		// expect post refresh
-		setResult(PostListActivity.RETURN_FROM_FULL_IMAGE, getIntent());
+		// setResult(PostListActivity.RETURN_FROM_FULL_IMAGE, getIntent());
 
 		// TODO Auto-generated method stub
 		super.onBackPressed();
--- src/com/athena/asm/view/TouchImageView.java
+++ src/com/athena/asm/view/TouchImageView.java
@@ -2,8 +2,7 @@
  * TouchImageView.java
  * By: Michael Ortiz
  * Updated By: Patrick Lackemacher
- * Modified By: Xi Lin
- * https://github.com/MikeOrtiz/TouchImageView
+ * Updated By: Babay88
  * -------------------
  * Extends Android ImageView to include pinch zooming and panning.
  */
@@ -11,11 +10,11 @@
 package com.athena.asm.view;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
@@ -23,7 +22,7 @@ import android.widget.ImageView;
 
 public class TouchImageView extends ImageView {
 
-    Matrix matrix = new Matrix();
+    Matrix matrix;
 
     // We can be in one of these 3 states
     static final int NONE = 0;
@@ -35,35 +34,36 @@ public class TouchImageView extends ImageView {
     PointF last = new PointF();
     PointF start = new PointF();
     float minScale = 1f;
-    float maxScale = 5f;
+    float maxScale = 3f;
     float[] m;
-    
-    float redundantXSpace, redundantYSpace;
-    
-    float width, height;
+
+
+    int viewWidth, viewHeight;
     static final int CLICK = 3;
     float saveScale = 1f;
-    float right, bottom, origWidth, origHeight, bmWidth, bmHeight;
-    
+    protected float origWidth, origHeight;
+    int oldMeasuredWidth, oldMeasuredHeight;
+
+
     ScaleGestureDetector mScaleDetector;
-    
+
     Context context;
 
     public TouchImageView(Context context) {
         super(context);
         sharedConstructing(context);
     }
-    
+
     public TouchImageView(Context context, AttributeSet attrs) {
-    	super(context, attrs);
-    	sharedConstructing(context);
+        super(context, attrs);
+        sharedConstructing(context);
     }
     
     private void sharedConstructing(Context context) {
-    	super.setClickable(true);
+        super.setClickable(true);
         this.context = context;
         mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
-        matrix.setTranslate(1f, 1f);
+        matrix = new Matrix();
         m = new float[9];
         setImageMatrix(matrix);
         setScaleType(ScaleType.MATRIX);
@@ -72,185 +72,164 @@ public class TouchImageView extends ImageView {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-            	mScaleDetector.onTouchEvent(event);
+                mScaleDetector.onTouchEvent(event);
+                PointF curr = new PointF(event.getX(), event.getY());
 
-            	matrix.getValues(m);
-            	float x = m[Matrix.MTRANS_X];
-            	float y = m[Matrix.MTRANS_Y];
-            	PointF curr = new PointF(event.getX(), event.getY());
-            	
-            	switch (event.getAction()) {
-	            	case MotionEvent.ACTION_DOWN:
-	                    last.set(event.getX(), event.getY());
-	                    start.set(last);
-	                    mode = DRAG;
-	                    break;
-	            	case MotionEvent.ACTION_MOVE:
-	            		if (mode == DRAG) {
-	            			float deltaX = curr.x - last.x;
-	            			float deltaY = curr.y - last.y;
-	            			float scaleWidth = Math.round(origWidth * saveScale);
-	            			float scaleHeight = Math.round(origHeight * saveScale);
-            				if (scaleWidth < width) {
-	            				deltaX = 0;
-	            				if (y + deltaY > 0)
-		            				deltaY = -y;
-	            				else if (y + deltaY < -bottom)
-		            				deltaY = -(y + bottom); 
-            				} else if (scaleHeight < height) {
-	            				deltaY = 0;
-	            				if (x + deltaX > 0)
-		            				deltaX = -x;
-		            			else if (x + deltaX < -right)
-		            				deltaX = -(x + right);
-            				} else {
-	            				if (x + deltaX > 0)
-		            				deltaX = -x;
-		            			else if (x + deltaX < -right)
-		            				deltaX = -(x + right);
-		            			
-	            				if (y + deltaY > 0)
-		            				deltaY = -y;
-		            			else if (y + deltaY < -bottom)
-		            				deltaY = -(y + bottom);
-	            			}
-                        	matrix.postTranslate(deltaX, deltaY);
-                        	last.set(curr.x, curr.y);
-	                    }
-	            		break;
-	            		
-	            	case MotionEvent.ACTION_UP:
-	            		mode = NONE;
-	            		int xDiff = (int) Math.abs(curr.x - start.x);
-	                    int yDiff = (int) Math.abs(curr.y - start.y);
-	                    if (xDiff < CLICK && yDiff < CLICK)
-	                        performClick();
-	            		break;
-	            		
-	            	case MotionEvent.ACTION_POINTER_UP:
-	            		mode = NONE;
-	            		break;
-            	}
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        last.set(curr);
+                        start.set(last);
+                        mode = DRAG;
+                        break;
+                        
+                    case MotionEvent.ACTION_MOVE:
+                        if (mode == DRAG) {
+                            float deltaX = curr.x - last.x;
+                            float deltaY = curr.y - last.y;
+                            float fixTransX = getFixDragTrans(deltaX, viewWidth, origWidth * saveScale);
+                            float fixTransY = getFixDragTrans(deltaY, viewHeight, origHeight * saveScale);
+                            matrix.postTranslate(fixTransX, fixTransY);
+                            fixTrans();
+                            last.set(curr.x, curr.y);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        mode = NONE;
+                        int xDiff = (int) Math.abs(curr.x - start.x);
+                        int yDiff = (int) Math.abs(curr.y - start.y);
+                        if (xDiff < CLICK && yDiff < CLICK)
+                            performClick();
+                        break;
+
+                    case MotionEvent.ACTION_POINTER_UP:
+                        mode = NONE;
+                        break;
+                }
+                
                 setImageMatrix(matrix);
                 invalidate();
-                return true; // indicate event was handled
+                return false; // indicate event was handled
             }
 
         });
     }
 
-    @Override
-    public void setImageBitmap(Bitmap bm) { 
-        super.setImageBitmap(bm);
-        if(bm != null) {
-        	bmWidth = bm.getWidth();
-        	bmHeight = bm.getHeight();
-        }
+    public void setMaxZoom(float x) {
+        maxScale = x;
     }
-    
-    @Override
-    public void setImageDrawable(Drawable d) {
-    	super.setImageDrawable(d);
-    	if(d != null) {
-        	bmWidth = d.getIntrinsicWidth();
-        	bmHeight = d.getIntrinsicHeight();
-        }
-    }
-    
-    public void setMaxZoom(float x)
-    {
-    	maxScale = x;
-    }
-    
+
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-    	@Override
-    	public boolean onScaleBegin(ScaleGestureDetector detector) {
-    		mode = ZOOM;
-    		return true;
-    	}
-    	
-		@Override
-	    public boolean onScale(ScaleGestureDetector detector) {
-			float mScaleFactor = detector.getScaleFactor();
-		 	float origScale = saveScale;
-	        saveScale *= mScaleFactor;
-	        if (saveScale > maxScale) {
-	        	saveScale = maxScale;
-	        	mScaleFactor = maxScale / origScale;
-	        } else if (saveScale < minScale) {
-	        	saveScale = minScale;
-	        	mScaleFactor = minScale / origScale;
-	        }
-        	right = width * saveScale - width - (2 * redundantXSpace * saveScale);
-            bottom = height * saveScale - height - (2 * redundantYSpace * saveScale);
-        	if (origWidth * saveScale <= width || origHeight * saveScale <= height) {
-        		matrix.postScale(mScaleFactor, mScaleFactor, width / 2, height / 2);
-            	if (mScaleFactor < 1) {
-            		matrix.getValues(m);
-            		float x = m[Matrix.MTRANS_X];
-                	float y = m[Matrix.MTRANS_Y];
-                	if (mScaleFactor < 1) {
-        	        	if (Math.round(origWidth * saveScale) < width) {
-        	        		if (y < -bottom)
-            	        		matrix.postTranslate(0, -(y + bottom));
-        	        		else if (y > 0)
-            	        		matrix.postTranslate(0, -y);
-        	        	} else {
-	                		if (x < -right) 
-	        	        		matrix.postTranslate(-(x + right), 0);
-	                		else if (x > 0) 
-	        	        		matrix.postTranslate(-x, 0);
-        	        	}
-                	}
-            	}
-        	} else {
-            	matrix.postScale(mScaleFactor, mScaleFactor, detector.getFocusX(), detector.getFocusY());
-            	matrix.getValues(m);
-            	float x = m[Matrix.MTRANS_X];
-            	float y = m[Matrix.MTRANS_Y];
-            	if (mScaleFactor < 1) {
-    	        	if (x < -right) 
-    	        		matrix.postTranslate(-(x + right), 0);
-    	        	else if (x > 0) 
-    	        		matrix.postTranslate(-x, 0);
-    	        	if (y < -bottom)
-    	        		matrix.postTranslate(0, -(y + bottom));
-    	        	else if (y > 0)
-    	        		matrix.postTranslate(0, -y);
-            	}
-        	}
-	        return true;
-	        
-	    }
-	}
-    
-    @Override
-    protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec)
-    {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        width = MeasureSpec.getSize(widthMeasureSpec);
-        height = MeasureSpec.getSize(heightMeasureSpec);
-        //Fit to screen.
-        float scale;
-        float scaleX =  (float)width / (float)bmWidth;
-        float scaleY = (float)height / (float)bmHeight;
-        scale = Math.min(scaleX, scaleY);
-        matrix.setScale(scale, scale);
-        setImageMatrix(matrix);
-        saveScale = 1f;
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
+            mode = ZOOM;
+            return true;
+        }
 
-        // Center the image
-        redundantYSpace = (float)height - (scale * (float)bmHeight) ;
-        redundantXSpace = (float)width - (scale * (float)bmWidth);
-        redundantYSpace /= (float)2;
-        redundantXSpace /= (float)2;
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            float mScaleFactor = detector.getScaleFactor();
+            float origScale = saveScale;
+            saveScale *= mScaleFactor;
+            if (saveScale > maxScale) {
+                saveScale = maxScale;
+                mScaleFactor = maxScale / origScale;
+            } else if (saveScale < minScale) {
+                saveScale = minScale;
+                mScaleFactor = minScale / origScale;
+            }
 
-        matrix.postTranslate(redundantXSpace, redundantYSpace);
+            if (origWidth * saveScale <= viewWidth || origHeight * saveScale <= viewHeight)
+                matrix.postScale(mScaleFactor, mScaleFactor, viewWidth / 2, viewHeight / 2);
+            else
+                matrix.postScale(mScaleFactor, mScaleFactor, detector.getFocusX(), detector.getFocusY());
+
+            fixTrans();
+            return true;
+        }
+    }
+
+    void fixTrans() {
+        matrix.getValues(m);
+        float transX = m[Matrix.MTRANS_X];
+        float transY = m[Matrix.MTRANS_Y];
         
-        origWidth = width - 2 * redundantXSpace;
-        origHeight = height - 2 * redundantYSpace;
-        right = width * saveScale - width - (2 * redundantXSpace * saveScale);
-        bottom = height * saveScale - height - (2 * redundantYSpace * saveScale);
-        setImageMatrix(matrix);
+        float fixTransX = getFixTrans(transX, viewWidth, origWidth * saveScale);
+        float fixTransY = getFixTrans(transY, viewHeight, origHeight * saveScale);
+
+        if (fixTransX != 0 || fixTransY != 0)
+            matrix.postTranslate(fixTransX, fixTransY);
+    }
+
+    float getFixTrans(float trans, float viewSize, float contentSize) {
+        float minTrans, maxTrans;
+
+        if (contentSize <= viewSize) {
+            minTrans = 0;
+            maxTrans = viewSize - contentSize;
+        } else {
+            minTrans = viewSize - contentSize;
+            maxTrans = 0;
+        }
+
+        if (trans < minTrans)
+            return -trans + minTrans;
+        if (trans > maxTrans)
+            return -trans + maxTrans;
+        return 0;
+    }
+    
+    float getFixDragTrans(float delta, float viewSize, float contentSize) {
+        if (contentSize <= viewSize) {
+            return 0;
+        }
+        return delta;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        viewWidth = MeasureSpec.getSize(widthMeasureSpec);
+        viewHeight = MeasureSpec.getSize(heightMeasureSpec);
+        
+        //
+        // Rescales image on rotation
+        //
+        if (oldMeasuredHeight == viewWidth && oldMeasuredHeight == viewHeight
+                || viewWidth == 0 || viewHeight == 0)
+            return;
+        oldMeasuredHeight = viewHeight;
+        oldMeasuredWidth = viewWidth;
+
+        if (saveScale == 1) {
+            //Fit to screen.
+            float scale;
+
+            Drawable drawable = getDrawable();
+            if (drawable == null || drawable.getIntrinsicWidth() == 0 || drawable.getIntrinsicHeight() == 0)
+                return;
+            int bmWidth = drawable.getIntrinsicWidth();
+            int bmHeight = drawable.getIntrinsicHeight();
+            
+            Log.d("bmSize", "bmWidth: " + bmWidth + " bmHeight : " + bmHeight);
+
+            float scaleX = (float) viewWidth / (float) bmWidth;
+            float scaleY = (float) viewHeight / (float) bmHeight;
+            scale = Math.min(scaleX, scaleY);
+            matrix.setScale(scale, scale);
+
+            // Center the image
+            float redundantYSpace = (float) viewHeight - (scale * (float) bmHeight);
+            float redundantXSpace = (float) viewWidth - (scale * (float) bmWidth);
+            redundantYSpace /= (float) 2;
+            redundantXSpace /= (float) 2;
+
+            matrix.postTranslate(redundantXSpace, redundantYSpace);
+
+            origWidth = viewWidth - 2 * redundantXSpace;
+            origHeight = viewHeight - 2 * redundantYSpace;
+            setImageMatrix(matrix);
+        }
+        fixTrans();
     }
 }
